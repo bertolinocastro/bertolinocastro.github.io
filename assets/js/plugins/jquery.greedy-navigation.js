240785d2880623a95d4557acd7a259045112d62a
bertolinocastro <bertolinocastro@gmail.com>
2020-07-18 17:40:56 -0300

    searchbar now fully functional with transitions and integrated with resultsbox, all responsive.

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -9,18 +9,17 @@ var $nav = $('#site-nav');
 var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
 var $hlinks = $('#site-nav .hidden-links');
+var $search = $('#masthead__search-input');
+var $result = $('.masterhead__gcse-searchresults-container');
 
-var breaks = [];
+var srfocs = false;
 
 function updateNav() {
 
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 14;
 
   // The visible list is overflowing the nav
   if($vlinks.width() > availableSpace) {
-
-    // Record the width of the list
-    breaks.push($vlinks.width());
 
     // Move item to the hidden list
     $vlinks.children().last().prependTo($hlinks);
@@ -28,28 +27,52 @@ function updateNav() {
     // Show the dropdown btn
     if($btn.hasClass('hidden')) {
       $btn.removeClass('hidden');
+      $btn.removeClass('close');
+    }
+
+    if($result.is(':visible')){
+      $btn.addClass('close');
+      $hlinks.removeClass('hidden');
+    }
+
+    // Show the list, if the input is opened
+    if(!$search.is(':focus') && (srfocs || $search[0] == $(document.activeElement)[0])){
+      $btn.addClass('close');
+      $hlinks.removeClass('hidden');
+      $search.addClass('masthead__search-notransition');
+      $search.focus();
+      $search[0].offsetHeight;
+      $search.removeClass('masthead__search-notransition');
     }
 
   // The visible list is not overflowing
   } else {
-
+    var extraSpace = $hlinks.children().length == 1 ? $btn.width()+14 : 0;
+    var consumedSp = $hlinks.children().length ? $hlinks.children().first().width() : 0;
     // There is space for another item in the nav
-    if(availableSpace > breaks[breaks.length-1]) {
+    if(availableSpace+extraSpace > $vlinks.width()+consumedSp) {
 
       // Move the item to the visible list
       $hlinks.children().first().appendTo($vlinks);
-      breaks.pop();
+
+      if(!$search.is(':focus') && (srfocs || $search[0] == $(document.activeElement)[0])){
+        $search.addClass('masthead__search-notransition');
+        $search.focus();
+        $search[0].offsetHeight;
+        $search.removeClass('masthead__search-notransition');
+      }
     }
 
     // Hide the dropdown btn if hidden list is empty
-    if(breaks.length < 1) {
+    if($hlinks.children().length == 0) {
       $btn.addClass('hidden');
       $hlinks.addClass('hidden');
+    }else if($result.is(':visible')){
+      $btn.addClass('close');
+      $hlinks.removeClass('hidden');
     }
-  }
 
-  // Keep counter updated
-  $btn.attr("count", breaks.length);
+  }
 
   // Recur if the visible list is still overflowing the nav
   if($vlinks.width() > availableSpace) {
@@ -65,8 +88,29 @@ $(window).resize(function() {
 });
 
 $btn.on('click', function() {
-  $hlinks.toggleClass('hidden');
-  $(this).toggleClass('close');
+  if($hlinks.hasClass('hidden')){
+    $(this).addClass('close');
+    $hlinks.removeClass('hidden');
+  }else{
+    $(this).removeClass('close');
+    $hlinks.addClass('hidden');
+    $search.blur();
+    srfocs = false;
+    $result.hide();
+    updateNav();
+  }
+});
+
+$search.focusin(function(){
+  srfocs = true;
+});
+
+$search.blur(function(){
+  srfocs = false;
+});
+
+$search.on('transitionend', function(){
+  updateNav();
 });
 
 updateNav();
